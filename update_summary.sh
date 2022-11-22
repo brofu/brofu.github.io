b7044e96bd265d75adc752950c3a11bd656e2ee9
brofu <brofu.talk@gmail.com>
2022-11-22 10:09:40 +0800

    update update_summary.sh to support more contents

--- update_summary.sh
+++ update_summary.sh
@@ -17,7 +17,7 @@ echo "Updating SMMMARY.md with Parameters: env $1, port $2"
 
 if [ "$1" == "live" ]; then
     echo "updating summary in live env"
-    NEW="https://brofu.github.io/notes-golang/"
+    NEW="https://brofu.github.io"
 else
     echo "updating summary in test env"
     NEW="http://localhost:$2/"
@@ -26,11 +26,23 @@ fi
 # list to update
 # to avoid to use hashmap in shell
 # update here
-topics=( "Golang" )
+topics=( "Golang,golang" "Elasticsearch,es")
 
 for i in "${topics[@]}"
 do
-   update_string "$i" $NEW SUMMARY.md 
+    #tms=$(echo "$i" | tr "," "\n")
+
+    # reference: https://stackoverflow.com/questions/10586153/how-to-split-a-string-into-an-array-in-bash
+    IFS=',' read -r -a tms <<< "$i"
+    topic=${tms[0]}
+
+    if [ "$1" == "live" ]; then
+    new="$NEW/notes-${tms[1]}"
+    else
+    new=$NEW
+    fi
+
+    update_string $topic $new SUMMARY.md
 done
 
 
